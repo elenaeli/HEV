d44cfb14cab264c8be6f4385ab29ed3eff67d577
Elena Elenkova <elenaeli@gmail.com>
2016-03-21 16:26:55 +0100

    Implemented the core
    Generated images for preliminary results presentation

--- src/core.m
+++ src/core.m
@@ -1,13 +1,11 @@
 function coreS = core(A)
-
-
 coreS = zeros(size(A,1),1);
 
 for i = 1:size(A,1)
     v1 = A(i,1);
     v2 = A(i,2);
     v12 = A(i,3);
-    if v1 + v2 <= v12
+    if v1 + v2 >= v12 && v1 >= v1 && v2 >= v2
         coreS(i) = 1;
     end
 end

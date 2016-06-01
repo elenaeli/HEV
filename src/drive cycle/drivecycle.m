b58fb31a14a260be6c00c3f8faceba62e05a6d28
Elena Elenkova <elenaeli@gmail.com>
2016-06-01 17:40:20 +0200

    Implemented Fuel Cons and Gas Emis for results
    Saved all scopes data to the workspace, increased decimation to 500,
    unchecked the take last 5000 points to prevent deletion of scope data at
    the end of the simulation. Experimented with abs and rel tolerances
    1e-4, 1e-8 gave an error at 273s

--- src/drive cycle/drivecycle.m
+++ src/drive cycle/drivecycle.m
@@ -1,10 +1,10 @@
 ftp75M = dlmread('data/ftp75_s_mph_reduced.txt','\t');
 ftp75 = struct('seconds', ftp75M(:,1), 'kph', ftp75M(:,2)* 1.60934);
-ftp75_1 = struct('seconds', ftp75M(1:30,1), 'kph', ftp75M(1:30,2)* 1.60934);
-ftp75_2 = struct('seconds', ftp75M(31:60,1), 'kph', ftp75M(31:60,2)* 1.60934);
-ftp75_3 = struct('seconds', ftp75M(61:90,1), 'kph', ftp75M(61:90,2)* 1.60934);
-ftp75_4 = struct('seconds', ftp75M(91:120,1), 'kph', ftp75M(91:120,2)* 1.60934);
-ftp75_5 = struct('seconds', ftp75M(121:end,1), 'kph', ftp75M(121:end,2)* 1.60934);
+ftp75_1 = struct('seconds', ftp75M(1:32,1), 'kph', ftp75M(1:32,2)* 1.60934);
+ftp75_2 = struct('seconds', ftp75M(32:62,1), 'kph', ftp75M(32:62,2)* 1.60934);
+ftp75_3 = struct('seconds', ftp75M(62:93,1), 'kph', ftp75M(62:93,2)* 1.60934);
+ftp75_4 = struct('seconds', ftp75M(93:123,1), 'kph', ftp75M(93:123,2)* 1.60934);
+ftp75_5 = struct('seconds', ftp75M(123:end,1), 'kph', ftp75M(123:end,2)* 1.60934);
 
 % mph to kph
 %ftp75.kph = ftp75.kph.* 1.60934;
@@ -19,13 +19,14 @@ accel = struct('seconds', accel(:,1), 'kph', accel(:,2));
 accel.time = 0.5* ((0+10) + (10-(0))*((2*accel.seconds - (1+21))/(21-(1))) ); 
 
 figure
+plot(ftp75.seconds, ftp75.kph);
 title('FTP75 drive cycle');
 xlabel('time (s)','FontSize',11)
 ylabel('speed (km/h)','FontSize',11)
-plot(ftp75.seconds, ftp75.kph)
 
 figure
+plot(nedc.seconds, nedc.kph);
 title('NEDC drive cycle');
 xlabel('time (s)','FontSize',11)
 ylabel('speed (km/h)','FontSize',11)
-plot(nedc.seconds, nedc.kph)
+
